49c5b26d7a5dccb1b4eace27a4eec2a9c7eb5a27
Liam <126953193+LiamHefford@users.noreply.github.com>
2025-04-03 13:01:52 +0100

    a lotta changes

--- enki/src/components/NavBar.tsx
+++ enki/src/components/NavBar.tsx
@@ -15,20 +15,24 @@ const NavBar: React.FC = () => {
   ];
 
   return (
-    <nav className="bg-gray-800/70 backdrop-filter backdrop-blur-sm text-white">
+    <nav className="bg-gray-800/30 backdrop-filter backdrop-blur-md border-b border-gray-700/30 shadow-lg text-white fixed w-full z-10">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center h-16">
           <div className="flex-shrink-0">
-            <Link href="/" className="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-600 dark:from-blue-400 dark:to-purple-400">
+            <Link 
+              href="/" 
+              className="font-bold text-xl bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-purple-400 hover:from-purple-400 hover:to-blue-400 transition-all duration-500 ease-in-out hover:[text-shadow:0_0_10px_rgba(147,51,234,0.7),0_0_15px_rgba(79,70,229,0.5)] px-2 py-1"
+            >
               Enki
             </Link>
           </div>
-          <div className="flex ml-6">
+          <div className="text-gray-400 mx-3">|</div>
+          <div className="flex">
             {navItems.map((item) => (
               <Link
                 key={item.title}
                 href={item.path}
-                className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700"
+                className="px-3 py-2 rounded-md text-sm font-medium hover:bg-gray-700/50 transition duration-150"
               >
                 {item.title}
               </Link>
